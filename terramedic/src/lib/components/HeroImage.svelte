ef7b9bd1c49eac5cb14a261f7bb9f24c81f97573
Leila Hadj-Chikh <lhadjchikh@users.noreply.github.com>
2026-03-01 16:17:28 -0500

    fix(HeroImage): remove overflow-hidden that caused e2e h1 visibility failure

--- terramedic/src/lib/components/HeroImage.svelte
+++ terramedic/src/lib/components/HeroImage.svelte
@@ -78,7 +78,7 @@
       {/if}
 
       {#if title || titleBrand}
-        <h1 class="mx-auto mb-6 max-w-3xl overflow-hidden font-bold text-white">
+        <h1 class="mx-auto mb-6 max-w-3xl font-bold text-white">
           <span class="block text-2xl md:text-4xl lg:text-5xl">{title}</span>
           {#if titleBrand}
             <span class="block text-5xl whitespace-nowrap sm:text-6xl md:text-7xl lg:text-8xl"
