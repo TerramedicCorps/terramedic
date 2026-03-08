9881de1fa808569e1f0f62a765dd86cfd005b5f2
Leila Hadj-Chikh <lhadjchikh@users.noreply.github.com>
2026-03-08 13:20:26 -0400

    refactor(frontend): remove unused category-colors config file

--- terramedic/src/lib/config/category-colors.ts
+++
@@ -1,6 +0,0 @@
-export const categoryColors: Record<string, { tagColor: string; buttonColor: string }> = {
-  donate: { tagColor: 'green', buttonColor: 'green' },
-  volunteer: { tagColor: 'blue', buttonColor: 'blue' },
-  resource: { tagColor: 'gold', buttonColor: 'gold' },
-  action: { tagColor: 'purple', buttonColor: 'purple' }
-};
