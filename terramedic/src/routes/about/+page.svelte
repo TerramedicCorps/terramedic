977322639e86fb2414a1aa2d39e4553b0075c28a
Leila Hadj-Chikh <lhadjchikh@users.noreply.github.com>
2026-03-14 13:33:06 -0400

    fix(about): remove Earth Optimism Summit link

--- terramedic/src/routes/about/+page.svelte
+++ terramedic/src/routes/about/+page.svelte
@@ -76,13 +76,7 @@
             rel="noopener noreferrer"
             class="text-terra-green hover:underline">Make for the Planet</a
           >
-          competition at the inaugural
-          <a
-            href="https://en.wikipedia.org/wiki/Earth_Optimism"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="text-terra-green hover:underline">Earth Optimism Summit</a
-          > in 2017.
+          competition at the inaugural Earth Optimism Summit in 2017.
         </p>
         <p class="mb-4 text-gray-300">
           The term is intentionally placed in the public domain under
