5663466a75884a7f6f1be78e16925cb0e67dbe6c
Leila Hadj-Chikh <lhadjchikh@users.noreply.github.com>
2026-03-15 14:21:55 -0400

    chore: document prerender + accept-language limitation
    Accept-language headers are empty at prerender time. No impact
    while content is English-only but will need revisiting for i18n.

--- terramedic/src/routes/volunteer/+page.server.ts
+++ terramedic/src/routes/volunteer/+page.server.ts
@@ -4,6 +4,8 @@ import { fetchOrganizations } from '$lib/server/api';
 export const prerender = true;
 
 export const load: PageServerLoad = async ({ request }) => {
+  // NOTE: accept-language is empty at prerender time. This is fine while
+  // content is English-only but will need revisiting when translations are added.
   const acceptLanguage = request.headers.get('accept-language') ?? undefined;
   try {
     const organizations = await fetchOrganizations('volunteer', acceptLanguage);
