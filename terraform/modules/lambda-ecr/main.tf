f3554e6f1f2e673fb26b29ac409fc1022b043a51
Leila Hadj-Chikh <lhadjchikh@users.noreply.github.com>
2026-03-15 14:46:02 -0400

    feat(infra): add ECR lifecycle rule to expire untagged images
    Reduces storage costs by automatically removing untagged images after 1 day.

--- terraform/modules/lambda-ecr/main.tf
+++ terraform/modules/lambda-ecr/main.tf
@@ -26,6 +26,19 @@ resource "aws_ecr_lifecycle_policy" "lambda" {
     rules = [
       {
         rulePriority = 1
+        description  = "Remove untagged images after 1 day"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 1
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
         description  = "Keep last 10 images"
         selection = {
           tagStatus   = "any"
